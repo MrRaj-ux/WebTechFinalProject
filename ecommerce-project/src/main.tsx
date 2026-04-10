4ac7e4f63004e602978ed0fe7370f22fa349f0d5
divyendu <rajdivyendu22@gmail.com>
2026-04-10 23:57:35 +0530

    Update frontend API URL to Render backend

--- ecommerce-project/src/main.tsx
+++ ecommerce-project/src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router';
+import axios from 'axios';
 import './index.css'
 import App from './App.tsx'
+
+axios.defaults.baseURL = 'https://webtechfinalproject-t3xz.onrender.com';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
